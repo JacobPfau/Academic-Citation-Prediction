38820763698d113f3df128696d482c40efc3ae9a
JacobPfau <jacob.pfau@gmail.com>
2018-11-26 20:56:14 +0100

    todict and get_k_nn

--- features_pairwise.py
+++ features_pairwise.py
@@ -4,9 +4,20 @@
 def Euc_Dist(X,Y):
 	return sum( [(X[i]-Y[i])**2 for i in range(len(X))] )**0.5
 
-def Max_Sim(node,list,features,n=5):
-	index_node = IDs.index(node)
-	indices = [IDs.index(node) for node in list]
+def Max_Sim(node,node_list,features,node_dict,n=5):
+	index_node = node_dict[node]
+	indices = [node_dict[node] for node in node_list]
 	d = sorted([Euc_Dist( features(index_node), features(i) ) for i in indices])
 	return d[:n]
 
+##############################################################
+#Papers with similar abstracts that also cite the target node
+
+def Get_K_NN(node,kdtree,node_dict,k_val=20):
+    """
+    @return: pair (indices,distances) of k_val nearest abstracts
+    """
+
+    index_node = node_dict[node]
+    dist, ind = kdtree.query(X[:1], k=k_val) 
+    return (ind,dist)
