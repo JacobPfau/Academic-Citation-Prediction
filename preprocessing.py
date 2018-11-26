b744689c7ffa840a487f04dcb00ed39acd243a24
JacobPfau <jacob.pfau@gmail.com>
2018-11-26 21:00:37 +0100

    with preprocessing.py to dict

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,16 @@
 ##############################################################
 #tfidf
 
-def tfidf(corpus,df=0.5,feats=10000:
+def tfidf(corpus,df=0.5,feats=10000):
 	vectorizer = Vectorizer(max_df=df, max_features=feats, stop_words='english')
 	X = vectorizer.fit_transform(corpus)
 
 	print ("n_samples: %d, n_features: %d" % X.shape)
 
     return X
+
+##############################################################
+#node dictionary
+
+def to_dict(node_info):
+	return dict(zip( [element[0] for element in node_info],range(len(node_info)) ))
