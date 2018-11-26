8a5f7e596faa9595218f05c387024db73f1119d4
LucasE <lucas.elbert@freenet.de>
2018-11-26 20:41:30 +0100

    pairwise: Added peer_popularity measure

--- features_pairwise.py
+++ features_pairwise.py
@@ -10,3 +10,16 @@ def Max_Sim(node,list,features,n=5):
 	d = sorted([Euc_Dist( features(index_node), features(i) ) for i in indices])
 	return d[:n]
 
+##############################################################
+# Peer popularity
+#
+# Estimates the "popularity" of node c in the surrounding of node n.
+# Returns the percentange of how many (back & forward) neighbours of n cite c?
+# Let N be all nodes w such that an edge (w,n) or (n,w) exists.
+# Return value: |{w in N s.t. exists edge (w,c)}| / |N|
+def peer_popularity(graph, source_ID, target_ID):
+    cites = 0
+    for w in graph.neighbors(source):
+        if graph.are_connected(w,target):
+            cites += 1
+    return cites/graph.neighborhood_size(source)
