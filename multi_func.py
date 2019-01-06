0f34632f39951617d0aef18df8ba21d51e8b1be3
JacobPfau <jacob.pfau@gmail.com>
2019-01-06 18:48:50 +0100

    added multifunc

---
+++ multi_func.py
@@ -0,0 +1,32 @@
+import features_pairwise as pw
+import preprocessing as prep
+
+class params:
+    
+    def  __init__(self,gold_graph, kdtree,l,node_dict,index_dict,pairs_subset_edges=True, 
+                          chunk_size=1000,to_do = {'succ_pred':True, 'Max_Sim':True, 'Citation_Check':True, 
+                                                   'node_degree':True, 'Reverse_Max_Sim':True}, k_cc=500, metric_ms='COS', n_ms=3):
+        self.gold_graph = gold_graph
+        self.kdtree = kdtree
+        self.l = l
+        self.node_dict = node_dict
+        self.index_dict = index_dict
+        self.pairs_subset_edges = True
+        self.chunk_size = chunk_size
+        self.to_do = to_do
+        self.k_cc = k_cc
+        self.metric_ms = metric_ms
+        self.n_ms = n_ms
+
+    
+    def by_chunk_noparams(self, pair_list):
+        out = pw.by_chunk(pair_list,self.gold_graph,self.kdtree,self.l,self.node_dict,self.index_dict,
+                         pairs_subset_edges=self.pairs_subset_edges, chunk_size = self.chunk_size,to_do = self.to_do, 
+                          k_cc=self.k_cc, metric_ms=self.metric_ms, n_ms=self.n_ms)
+        return out
+    
+    def all_paths_noparams(self, pairs_list):
+        return prep.all_paths(pairs_list, self.gold_graph)
+    
+    def test(self, input_list):
+        return input_list+2
